a9cd7db5da6ae59608963f0a7c82d2cc7cb70ba0
CP342138 <KaneloSefoloko@capitecbank.co.za>
2020-07-30 23:20:11 +0200

    enter input

--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -1,16 +1,31 @@
 package main.java;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Roulette {
 
     public static void main (String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int betChoice;
+        double amountToBet = 0;
+
 
         ReadFromFile getFile = new ReadFromFile();
         List<String> nameList = getFile.readFromFile();
 
         //Print out names from nameList
         nameList.forEach(System.out::println);
+
+        for (String name : nameList) {
+            betChoice = input.nextInt();
+            amountToBet = input.nextDouble();
+
+            System.out.println(name + betChoice + amountToBet);
+        }
+
+
     }
 }
