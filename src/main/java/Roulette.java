0f079f468ab684526c75c34d38435a7cce9c13f2
CP342138 <KaneloSefoloko@capitecbank.co.za>
2020-07-31 00:39:04 +0200

    done

--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -11,7 +11,8 @@ public class Roulette {
     public static void main (String[] args) throws IOException, InterruptedException {
         Scanner input = new Scanner(System.in);
         Random random = new Random();
-        int betChoice, number;
+        int betChoice, number, spin = 0;
+        int result;
         double amountToBet = 0;
         String numberType = null;
 
@@ -53,7 +54,34 @@ public class Roulette {
             // Generate random number in range 0 to 36
             Thread.sleep(30000);
             int rand_int1 = random.nextInt(37);
+            spin++;
             System.out.println(rand_int1);
+
+            if (betChoice == 2) {
+                if (rand_int1 == number) {
+                    result = 36;
+                } else {
+                    result = 0;
+                }
+            } else {
+                if (rand_int1 == 0 || rand_int1 % 2 != betChoice) {
+                    result = 2;
+                } else {
+                    result = 2;
+                }
+            }
+
+            if (result > 0) {
+                System.out.println("Player \t" + "Bet\t" + "Outcome\t" + "Winnings");
+                System.out.println("----");
+                System.out.println(name + "\t" + betChoice + "\t" + "WIN" + " " + result * amountToBet);
+
+            } else {
+                System.out.println("Player \t" + "Bet\t" + "Outcome\t" + "Winnings");
+                System.out.println("----");
+                System.out.println(name + "\t" + betChoice + "\t" + "LOSE" + " " +result * amountToBet);
+
+            }
 
         }
 
