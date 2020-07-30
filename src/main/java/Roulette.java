7d08c816d76ee19c1748d8ced6d0413105ee10bf
CP342138 <KaneloSefoloko@capitecbank.co.za>
2020-07-30 23:30:54 +0200

    check number Type

--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -11,6 +11,9 @@ public class Roulette {
         Scanner input = new Scanner(System.in);
         int betChoice;
         double amountToBet = 0;
+        String numberType = null;
+
+        final String BARBARA = "Barbara";
 
 
         ReadFromFile getFile = new ReadFromFile();
@@ -23,7 +26,17 @@ public class Roulette {
             betChoice = input.nextInt();
             amountToBet = input.nextDouble();
 
-            System.out.println(name + betChoice + amountToBet);
+
+            if (betChoice % 2 == 0 && name.equalsIgnoreCase(BARBARA)) {
+                numberType = "Even";
+                System.out.println(name + " " + numberType + " " + amountToBet);
+            } else if (betChoice % 2 != 0 && name.equalsIgnoreCase(BARBARA)){
+                numberType = "Odd";
+                System.out.println(name + " " + numberType + " " + amountToBet);
+            } else {
+
+                System.out.println(name + " " + betChoice + " " + amountToBet);
+            }
         }
 
 
