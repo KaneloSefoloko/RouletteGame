337d47402666d14d2f73e19ae3c85c59690c335c
CP342138 <KaneloSefoloko@capitecbank.co.za>
2020-07-30 23:42:25 +0200

    generate random number

--- src/main/java/Roulette.java
+++ src/main/java/Roulette.java
@@ -3,12 +3,14 @@ package main.java;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Roulette {
 
     public static void main (String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
+        Random random = new Random();
         int betChoice;
         double amountToBet = 0;
         String numberType = null;
@@ -37,6 +39,10 @@ public class Roulette {
 
                 System.out.println(name + " " + betChoice + " " + amountToBet);
             }
+
+            // Generate random number in range 0 to 36
+            int rand_int1 = random.nextInt(37);
+            System.out.println(rand_int1);
         }
 
 
